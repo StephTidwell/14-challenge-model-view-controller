3284cd1bdf375ae13c5fce28a4f6d692fc75b048
Stephanie Tidwell <stephanie.tidwell.civ@gmail.com>
2023-03-05 14:26:53 -0500

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize");
+
+require("dotenv").config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    // process.env.DB_MYSQLDIALECT,
+    // process.env.DB_PORT,
+    // process.env.DB_HOST,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      dialectOptions: {
+        socketPath: "/private/tmp/mysql.sock",
+      },
+      // port: 3306,
+      user: "root",
+      password: "password",
+      database: "mvc_blog_db",
+    }
+  );
+}
+
+module.exports = sequelize;
